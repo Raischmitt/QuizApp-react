f72535fe9dfec230d7dc882b8e010493798c35cb
Raíssa <raissajoaquim29@gmail.com>
2025-07-07 22:31:07 -0300

    fazendo o resumo final

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react';
 
 import QUESTIONS from '../question.js';
 import Question from './Question.jsx';
-import quizCompleteImg from '../assets/quiz-complete.png';
+import Summary from './Summary.jsx';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
@@ -11,22 +11,19 @@ export default function Quiz() {
     const quizComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-       
+
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
-    }, 
-    []);
+    },
+        []);
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (quizComplete) {
-        return <div id="summary">
-            <img src={quizCompleteImg} alt="trophy icon" />
-            <h2>Quiz Complete!</h2>
-        </div>
-
-    };
+        return <Summary userAnswers={userAnswers} />
+    
+    }
 
     return (
         <div id="quiz">
